a384e144e303c5b23a26166c9014b623122cc91d
elonehoo <hi@elonehoo.me>
2023-02-05 17:48:29 +0800

    feat: add svelte play

---
+++ playground/svelte/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import PKG from 'vite-plugin-package-configs'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    PKG()
+  ],
+})
