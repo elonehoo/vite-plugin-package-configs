635d0b45efa60d263ec1d102cfb6aa9ca50d562a
elonehoo <hi@elonehoo.me>
2023-02-05 17:36:53 +0800

    feat: add preact play

---
+++ playground/preact/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import PKG from 'vite-plugin-package-configs'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    preact(),
+    PKG()
+  ],
+})
